5c62a5b94ebe7934014a9df82169d3c8a165d5e4
Meghana-ch <54450970+Meghana-ch@users.noreply.github.com>
2019-08-25 09:34:07 +0530

    automated door open and close
    using ultrasonic sensor it senses anyone standing a meter away, the door opens and once the person is not detected in the area the door automatically closes.

---
+++ ishan.ino
@@ -0,0 +1,55 @@
+#include<Servo.h>
+Servo s;
+const int trig=5;
+const int echo=3;
+  long dur;
+  int dist;
+void setup() 
+{
+pinMode(trig, OUTPUT);
+pinMode(echo, INPUT);
+Serial.begin(9600);
+s.attach(11);
+
+
+  
+
+}
+
+void loop()
+{
+ int i;
+ digitalWrite(trig,LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig,LOW);
+
+  dur= pulseIn(echo,HIGH);
+
+  dist=dur*0.034/2;
+ Serial.print("Distance: ");
+  Serial.print(dist);
+  delay(100);
+   
+  if(dist<=25)
+  {
+  
+   for(i=0;i<180;i++)
+ {
+  s.write(i);
+  delay(50);
+  
+  } }
+  else if(dist>=26)
+  {
+    for(i=180;i>0;i--)
+    {
+  
+  s.write(i);
+  delay(50);
+    }
+  }
+  
+}
